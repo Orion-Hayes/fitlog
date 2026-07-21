051417073693bd2cf0c2e43d8ab34b49ebe705b5
Orion-Hayes <kurnagm026@gmail.com>
2026-07-21 10:14:57 +0800

    Create cloud-config.js

---
+++ fitlog/cloud-config.js
@@ -0,0 +1,6 @@
+// The browser-safe project URL and anonymous key are added here during setup.
+// Never put a Supabase service_role key in this file.
+window.FITLOG_SUPABASE_CONFIG = {
+  url: "https://kecjjnvrgyneqtjxerwz.supabase.co",
+  anonKey: "sb_publishable_QOv4-Gttnz4D2cdRuy0D3A_vgbO80CD",
+};
